e9ae61cfdb80bb479aa1b11413d32c25cfacd515
Étienne Plante <zerox029@gmail.com>
2020-12-02 23:18:30 -0500

    :tada: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Particles from "react-tsparticles";
-import logo from './logo.svg';
 import './App.css';
 import particlesOptions from "./particles.json";
 
@@ -8,31 +7,9 @@ function App() {
     return (
         <div className="App">
             <Particles options={particlesOptions}/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.matteobruni.it"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
+            <h1>Etienne Plante</h1>
+            <p>Sit qui exercitation sint Lorem deserunt. Eu excepteur occaecat veniam ut dolor mollit exercitation eiusmod reprehenderit. Occaecat ipsum deserunt consectetur laboris aliquip. Fugiat mollit excepteur ad aute cupidatat Lorem excepteur officia.</p>
+            <p>Reprehenderit cupidatat exercitation occaecat occaecat et ea. Ea consequat nostrud consectetur nostrud irure exercitation aliquip fugiat. Labore labore in reprehenderit exercitation est et voluptate commodo incididunt in non. Laboris veniam Lorem id magna deserunt. Sit nisi exercitation velit minim. Fugiat sit dolor in nostrud ullamco ut ex non et cupidatat reprehenderit.</p>
         </div>
     );
 }
