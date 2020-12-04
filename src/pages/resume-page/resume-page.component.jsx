57b3d6629072550e68cdbb8d260061e0a7c512b9
Étienne Plante <zerox029@gmail.com>
2020-12-03 23:50:44 -0500

    :construction: Started work on resume page

--- src/pages/resume-page/resume-page.component.jsx
+++ src/pages/resume-page/resume-page.component.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import TextTransition, { presets } from 'react-text-transition';
 
-const Resume = () => {
-  return (
-    <div>
-      Resume page
-    </div>
-  )
+import { EDUCATION } from './resume.data';
+
+import './resume-page.styles.css';
+
+class Resume extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      eduIndex: 0,
+    }
+  }
+
+  render() {
+    return (
+      <div className="resume">
+        <div className="education">
+          <h2>Education</h2>
+          <div className="education-item" onClick={() => { this.setState({eduIndex: this.state.eduIndex + 1}) }}>
+            <b><TextTransition text={EDUCATION[this.state.eduIndex % EDUCATION.length].type} springConfig={ presets.gentle } inline /></b>
+            <i><TextTransition text={EDUCATION[this.state.eduIndex % EDUCATION.length].school} springConfig={ presets.gentle } inline /></i>
+            <i><TextTransition text={EDUCATION[this.state.eduIndex % EDUCATION.length].dates} springConfig={ presets.gentle } inline /></i>
+          </div>
+        </div>
+        <div className="professional-experience">
+          <h2>Professional Experience</h2>
+          <div className="professional-experience-item">
+            <b>Programming internship</b>
+            <i>CREO inc / Montréal, QC / 2017 (Summer)</i>
+            <ul>
+              <li>Develop software capable of emulating electrical circuitry</li>
+              <li>Program in Unity with the C# language</li>
+              <li>Integrate and debug external software</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Resume;
---
+++ src/pages/resume-page/resume.data.js
@@ -0,0 +1,12 @@
+export const EDUCATION = [
+  {
+    type: "High school diploma",
+    school: "Collège Durocher Saint-Lambert",
+    dates: "2014 - 2019"
+  },
+  {
+    type: "Diploma of college studies",
+    school: "Dawson College",
+    dates: "2019 - ..."
+  },
+];
